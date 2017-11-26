dc07fa441989d44b10cb745dbcb75bdb92ae8bd3
A <diblo28@yahoo.fr>
2017-11-26 11:18:24 -0500

    Contacts: Animations Reload Rows in UITableView (Ep 2)

--- Connect/Connect/ViewController.swift
+++ Connect/Connect/ViewController.swift
@@ -8,32 +8,49 @@
 
 import UIKit
 
- let cellID = "cellID"
+let cellID = "cellID"
 class ViewController: UITableViewController {
-
-
     
-     let twoDimensionalArray = [
-     [ "Jack", "Steve", "Mark", "John", "Alice", "Elyse"],
-     ["Carl", "Chris", "Christina", "Cameron", "Carnesie"],
-     ["David", "Don","Denise", "Devon", "Darrel", "Delphine"]
+    
+    
+    let twoDimensionalArray = [
+        [ "Jack", "Steve", "Mark", "John", "Alice", "Elyse"],
+        ["Carl", "Chris", "Christina", "Cameron", "Carnesie"],
+        ["David", "Don","Denise", "Devon", "Darrel", "Delphine"]
     ]
+    
+    var showIndexpaths = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.title = "Contact"
-         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.backgroundColor = .red
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Show IndexPath", style: .plain, target: self, action: #selector(handleIndexPath))
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
-        
-        
     }
-
+    
+    @objc func handleIndexPath(){
+        var indexpathToreload = [IndexPath]()
+        for section in twoDimensionalArray.indices {
+            for  row in twoDimensionalArray[section].indices{
+                let indexpath = IndexPath(row: row , section: section)
+                indexpathToreload.append(indexpath)
+            }
+        }
+        showIndexpaths = !showIndexpaths
+        let animationStyle = showIndexpaths ? UITableViewRowAnimation.right : .left
+        tableView.reloadRows(at: indexpathToreload, with: animationStyle)
+    }
+    
+    
+    
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let  header = UILabel()
-         header.text = "Header"
+        header.text = "Header"
         header.backgroundColor = .lightGray
-         return header
+        return header
     }
     override func numberOfSections(in tableView: UITableView) -> Int {
         return twoDimensionalArray.count
@@ -45,8 +62,11 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell =  tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
         let name = twoDimensionalArray[indexPath.section][indexPath.row]
-        
+        cell.textLabel?.text = "\(name)"
+    
+        if showIndexpaths{
         cell.textLabel?.text = "\(name) Section: \(indexPath.section) Row: \(indexPath.row)"
+        }
         return cell
     }
 }
